2889ac7b5314372f610d33654af24f912acc35d3
WLTCarlos <wltc@outlook.es>
2021-07-12 16:07:38 -0500

    Eliminación de comentarios

--- Ejercicios.java
+++ Ejercicios.java
@@ -12,14 +12,14 @@ public class Ejercicios {
         int dia = Integer.parseInt(fn1[0]);
         int mes = Integer.parseInt(fn1[1]);     //Transforma en enteros las 3 cadenas resultantes.
         int año = Integer.parseInt(fn1[2]);
-        var sum = dia + mes + año;              //Se suman los 3 numeros.
+        var sum = dia + mes + año;              //Se suman los 3 números.
         
         var res = 0;
         while(sum > 0){
             res = res + sum % 10;               //A través de la operación módulo se separan y suman suman los numeros.
             sum /= 10;
         }
-        System.out.println("Su numero de la suerte es: " + res);
+        System.out.println("Su número de la suerte es: " + res);
     }
 
     
